44b25c8f094847ea46f70a23d83b1e209b898418
git-yash-dev <rajyash0701@gmail.com>
2024-10-14 19:25:40 +0530

    normal commit

---
+++ C/areacircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    float r;
+    printf("enter radius:");
+    scanf ("%f",&r);
+    float pi=3.14;
+    float area=pi*r*r;
+    printf("area of circle is :%f\n", area);
+}
